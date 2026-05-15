1b370e57f5e0e15768713bb354c9789bce38bac8
9bikash <bikasha371@gmail.com>
2026-05-15 16:08:07 +0930

    Added main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+# main.py
+      
+
+from app import SpotTheDifferenceApp
+
+
+if __name__ == "__main__":
+    # Create  and run the  applications
+    app = SpotTheDifferenceApp()
+    app.mainloop()
